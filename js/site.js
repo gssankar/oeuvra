0c16f20b14d34a96910ba1fe0939682c0c84149f
gssankar <g.s.sankar@student.tudelft.nl>
2020-12-29 15:16:42 +0530

    Included description

--- js/site.js
+++ js/site.js
@@ -53,6 +53,7 @@ $(window).load(function () { // makes sure the whole site is loaded
 	$('#status').fadeOut(); // will first fade out the loading animation
 	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
 	$('body').delay(350).css({'overflow-y': 'visible'});
+	$('body').toggleClass('notransition'); // kills all transition and animation under .notransition
 
 	/* WOW Elements */
 	if (typeof WOW == 'function') {
